6777485092c731b83e883d21ac04933f39b40e4a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-01-30 09:13:50 +0200

    update

--- chapter26/app/api/quotes/route.js
+++ chapter26/app/api/quotes/route.js
@@ -1,19 +1,22 @@
-import quotes from './data.json';
-import { NextResponse } from 'next/server';
-import {v4 as uuidv4} from 'uuid';
+import { prisma } from "../../db";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(req) {
-    return NextResponse.json(quotes);
-};
+  const quotes = await prisma.quote.findMany();
+  console.log("GET books called");
+
+  return NextResponse.json(quotes);
+}
 
 export async function POST(req) {
-    const {quote, by} = await req.json();
-    const newQuote = {
-        id: uuidv4(),
-        quote,
-        by
-    };
-    quotes.push(newQuote);
+  const { quote, by } = await req.json();
+  const newQuote = {
+    id: uuidv4(),
+    quote,
+    by,
+  };
+  quotes.push(newQuote);
 
-    return NextResponse.json("Quote added successfully");
+  return NextResponse.json("Quote added successfully");
 }
